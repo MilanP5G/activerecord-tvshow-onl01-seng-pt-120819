e3fda6075600c95489ac3c1c78537e57142db0fe
Milan Parmar <milan_parmar@hotmail.co.uk>
2020-03-10 16:51:46 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,6 +14,10 @@ class Show < ActiveRecord::Base
   
   def self.least_popular_show
     self.where("rating = ?", self.lowest_rating).first
+  end
+  
+  def self.ratings_sum
+    self.where("rating = ?", self.sum)
   end 
   
 end 
